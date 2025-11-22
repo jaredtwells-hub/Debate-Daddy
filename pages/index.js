a770647c627ca4d1cfcdbd86a45b80fc607f2eb5
jaredtwells-hub <jaredtwells@gmail.com>
2025-11-22 05:57:47 -0800

    Create Home component for Debate Daddy API

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Debate Daddy API</h1>
+      <p>The backend is running. Try POSTing to /api/analyze.</p>
+    </div>
+  );
+}
